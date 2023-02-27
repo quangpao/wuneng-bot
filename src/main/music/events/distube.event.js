cf9f99021a370b47bb31673c04dd6f57a2317a18
quangpao <ddquangbao@gmail.com>
2023-02-27 22:59:11 +0700

    folder re-construct and addSong Embed

--- src/main/music/events/distube.event.js
+++ src/main/music/events/distube.event.js
@@ -1,5 +1,5 @@
 const WNClient = require("../../../common/classes/WNClient");
-const PlayBuilder = require("../builders/play.builder");
+const PlayBuilder = require("../builders/embeds/distube-event.embed");
 
 /**
  *
@@ -9,7 +9,82 @@ module.exports = (client) => {
   client.distube.on("playSong", (queue, song) => {
     console.log(queue, song);
     queue.textChannel.send({
-      embeds: [PlayBuilder.embedBuilder(song, song.member)],
+      embeds: [PlayBuilder.PlaySong(song)],
     });
   });
+
+  // client.distube.on("addList", (queue, playlist) => {
+  //   console.log(queue, playlist);
+  //   queue.textChannel.send({
+  //     embeds: [PlayBuilder.AddList(playlist)]
+  //   })
+  // })
+
+  client.distube.on("addSong", (queue, song) => {
+    console.log(queue, song)
+    queue.textChannel.send({
+      embeds: [PlayBuilder.AddSong(song)]
+    })
+  })
+/*
+  client.distube.on("deleteQueue", (queue) => {
+    console.log(queue)
+    // TODO
+  })
+
+  client.distube.on("disconnect", (queue) => {
+    console.log(queue)
+    // TODO
+  })
+
+  client.distube.on("finish", (queue) => {
+    console.log(queue)
+    // TODO
+  })
+
+  client.distube.on("empty", (queue) => {
+    console.log(queue)
+    // TODO
+  })
+
+  client.distube.on("error", (channel, error) => {
+    console.log(channel, error)
+    // TODO
+  })
+
+  client.distube.on("noRelated", (queue) => {
+    console.log(queue)
+    // TODO
+  })
+
+  client.distube.on("initQueue", (queue) => {
+    console.log(queue)
+    // TODO
+  })
+
+  client.distube.on("searchCancel", (message, query) => {
+    console.log(message, query)
+    // TODO
+  })
+
+  client.distube.on("searchDone", (message, answer, query) => {
+    console.log(message, answer, query)
+    // TODO
+  })
+
+  client.distube.on("searchInvalidAnswer", (message, answer, query) => {
+    console.log(message, answer, query)
+    // TODO
+  })
+
+  client.distube.on("searchNoResult", (message, query) => {
+    console.log(message, query)
+    // TODO
+  })
+
+  client.distube.on("searchResult", (message, results, query) => {
+    console.log(message, results, query)
+    // TODO
+  })
+  */
 };
